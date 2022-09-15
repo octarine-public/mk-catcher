a1547700b8853ea38ba7dae3490798718d505762
SicdeX <sicdex@gmail.com>
2022-09-15 14:42:08 +0300

    fixed translate

---
+++ Translate.ts
@@ -0,0 +1,11 @@
+import { Menu, Utils } from "github.com/octarine-public/wrapper/index"
+
+const path = "github.com/octarine-public/mk-catcher/scripts_files"
+const Load = (name: string) => {
+	return new Map<string, string>
+		(Object.entries(Utils.readJSON(`${path}/translate/${name}.json`)))
+}
+
+Menu.Localization.AddLocalizationUnit("russian", Load("ru"))
+Menu.Localization.AddLocalizationUnit("english", Load("en"))
+Menu.Localization.AddLocalizationUnit("сhinese", Load("cn"))
